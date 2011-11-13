deb7869a3da46a30d191c56c25de3fb0bb43ab81
Gloria Budiman <gbudiman@purdue.edu>
2011-11-13 04:16:44 -0500

    Able to find use-distance

--- src/tinyAssembler.java
+++ src/tinyAssembler.java
@@ -5,6 +5,7 @@ class tinyAssembler {
 	public List<Integer> bbIndex;
 	public Vector<Vector<String>> rxm;
 	public Vector<String> rUsed;
+	public Vector<Vector<Integer>> distantUse;
 
 	public tinyAssembler(List<String> _tiny, List<Integer> _bbIndex) {
 		instruction = _tiny;
@@ -16,8 +17,10 @@ class tinyAssembler {
 					+ bbIndex.size());
 		}
 		rxm = new Vector<Vector<String>>();
+		distantUse = new Vector<Vector<Integer>>();
 		for (int i = 0; i < instruction.size(); i++) {
 			rxm.add(new Vector<String>());
+			distantUse.add(new Vector<Integer>());
 		}
 	}
 
@@ -56,11 +59,61 @@ class tinyAssembler {
 			}
 			rxm.setElementAt(vd, i);
 		}
+
+		preAllocate();
+	}
+
+	public void preAllocate() {
+		for (int i = 0; i < instruction.size(); i++) {
+			if (rxm.get(i).size() > 4) {
+				Vector<Integer> dul = new Vector<Integer>();	
+				for (int j = 0; j < rxm.get(i).size(); j++) {
+					
+					dul.add(getUseDistance(i, rxm.get(i).get(j).trim()));
+				}
+				distantUse.setElementAt(dul, i);
+			}
+		}
+	}
+
+	public int getUseDistance(int startPoint, String _target) {
+		int found = -1;
+		int iteration = startPoint + 1;
+		while (found == -1 && iteration < instruction.size()) {
+			//System.out.println("Searching at " + instruction.get(iteration));
+			String[] ss = instruction.get(iteration++).split("\\s");
+			if (ss[0].startsWith("add")
+				|| ss[0].startsWith("sub")	
+				|| ss[0].startsWith("mul")	
+				|| ss[0].startsWith("div")) {
+				if (ss[1].equals(_target)) {
+					found = 1;
+				}
+			}
+			else if (ss[0].startsWith("cmp")) {
+				if (ss[1].equals(_target) || ss[2].equals(_target)) {
+					found = 1;
+				}
+			}
+			else if (ss[0].startsWith("move")) {
+				if (ss[1].equals(_target)) {
+					found = 1;
+				}
+			}
+		}
+		
+		if (found == -1) {
+			System.out.println(";WARNING! Future use of " + _target + " not found");
+			return -1;
+		}
+		return iteration - startPoint;
 	}
 
 	public void printOut(int verbosity) {
-		int spacer = 40;
+		int spacer = 20;
+		int analysisSpace = 32;
 		int whitespaceCount;
+		int registerCount;
 		for (int i = 0; i < instruction.size(); i++) {
 			if (verbosity == 0 && instruction.get(i).startsWith(";")) {
 				continue;
@@ -70,9 +123,14 @@ class tinyAssembler {
 			for (int j = 0; j < whitespaceCount; j++) {
 				System.out.print(" ");
 			}
-			System.out.print("; " + bbIndex.get(i) + " {");
+			registerCount = analysisSpace - rxm.get(i).toString().length();
+			System.out.print("; " + bbIndex.get(i) + " ");
 			System.out.print(rxm.get(i).toString());
-			System.out.println("} " + i);
+			for (int j = 0; j < registerCount; j++) {
+				System.out.print(" ");
+			}
+			System.out.print(distantUse.get(i).toString());
+			System.out.println();
 		}
 	}
 }
